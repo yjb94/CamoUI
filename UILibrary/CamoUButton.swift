7f3029112bf2a6fd2f35858c52d5a59a9f581ae5
Camo_u <jbyun94@naver.com>
2016-12-28 00:49:15 +0900

    Initial Commit

---
+++ UILibrary/CamoUButton.swift
@@ -0,0 +1,21 @@
+//
+//  CamoUButton.swift
+//  UILibrary
+//
+//  Created by Camo_u on 2016. 12. 28..
+//  Copyright © 2016년 Camo_u. All rights reserved.
+//
+
+import UIKit
+
+class CamoUButton: UIButton
+{
+    init(frame: CGRect, url:String="")
+    {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+}
---
+++ UILibrary/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  UILibrary
+//
+//  Created by Camo_u on 2016. 12. 28..
+//  Copyright © 2016년 Camo_u. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController
+{
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning()
+    {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
